8ff44554924a435b57cb416c78f709d845cce432
Durgaprasad <125672017+durgaprasad-04@users.noreply.github.com>
2025-10-30 22:17:12 +0530

    Add files via upload

---
+++ local/LocalExample.java
@@ -0,0 +1,15 @@
+package local;
+
+public class LocalExample {
+
+	void show() {
+		int a=10;
+		System.out.println("Local variable a="+a);
+	}
+	public static void main(String[] args) {
+		LocalExample e = new LocalExample();
+		e.show();
+
+	}
+
+}
