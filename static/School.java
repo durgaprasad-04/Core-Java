5ccc1e259d38f7107d2c73574c02a4d65d40c05e
Durgaprasad <125672017+durgaprasad-04@users.noreply.github.com>
2025-11-02 18:54:53 +0530

    Add files via upload

---
+++ static/School.java
@@ -0,0 +1,19 @@
+package statics;
+
+public class School {
+	String name;
+	int id;
+	static String school="Anugraha";
+	void students() {
+		System.out.println("Student name"+name+",ID:"+id+",school name"+school);
+	}
+	public static void main(String[] args) {
+		School s = new School();
+		s.name="Prasad";
+		s.id=179;
+		s.students();
+		s.students(School.school);
+
+	}
+
+}
